c7c063fe13dcc8b277b90d471bdc2828f0386e70
notBroman <notbroman@gmail.com>
2021-03-30 17:15:30 +0200

    use regex to find more incorrect expressions

--- expression_tree.py
+++ expression_tree.py
@@ -4,6 +4,33 @@ import json
 from ADT import tree_base
 from ADT import stack
 
+class OperandsError(Exception):
+    pass
+
+class BracketsError(Exception):
+    pass
+
+class OperatorsError(Exception):
+    pass
+
+def miss_op(string:str):
+    p1 = re.compile("\([0-9]\(")
+    p2 = re.compile("\)[0-9]\)")
+    p3 = re.compile("\([0-9][\+\-\*\/]\)")
+    p4 = re.compile("\([\+\-\*\/][0-9]\)")
+
+    f1 = re.search(p1, string)
+    f2 = re.search(p2, string)
+    f3 = re.search(p3, string)
+    f4 = re.search(p4, string)
+
+    if f1 is not None or f2 is not None:
+        raise OperatorsError("invalid expression: missing operator")
+    elif f3 is not None or f4 is not None:
+        raise OperandsError("invalid expression: missing operant")
+    else:
+        pass
+
 def num_brackets(string:str):
     """ there need to be twice as many brackets as operators """
     """ otherwise there is more than 1 operator in bracket pair """
@@ -11,12 +38,9 @@ def num_brackets(string:str):
     brackets = [str(e) for e in string if e == "(" or e == ")"]
     operators = [str(e) for e in string if e == "+" or e == "-" or e == "*" or e == "/"]
 
-    if len(brackets) == 2*len(operators):
-        # return True if there are enough brackets
-        return True
-    else:
-        # return False if there is too little brackets
-        return False
+    if len(brackets) != 2*len(operators):
+        # raise operands error if there are the wrong number of brackets/operators
+        raise OperandsError("invalid expression: wrong number of operands")
 
 
 def match_bracket(st1:stack, string:str):
@@ -32,23 +56,22 @@ def match_bracket(st1:stack, string:str):
                 st1.pop()
             except IndexError:
                 # an index error is raised if we try to pop from an empty stack
-                return False
-    if len(st1) == 0:
+                raise BracketsError("invalid expression: mismatched brackets")
+    if len(st1) != 0:
         # return True if all opend brackets were closed
-        return True
-    else:
-        # return False if brackets are missmatched
-        return False
+        raise BracketsError("invalid expression: mismatched brackets")
 
 def validate(string:str):
     s1 = stack.Stack()
-    brac_val = match_bracket(s1, string)
-    if brac_val == False:
-        print("invalid expression: brackets do not match\n")
+    try:
+        match_bracket(s1, string)
+        num_brackets(string)
+        miss_op(string)
+    except Exception as e:
+        print(e)
+        return False
     else:
-        # check if expression follows X?Y recursively or not
-        # TODO
-        print("brackets match \n")
+        return True
 
 
 def menu():
@@ -68,6 +91,12 @@ def menu():
 
 if __name__ == '__main__':
     a = '(((5 + 2) * (2 - 1))/((2 + 9) + ((7 - 2) - 1)) * 8)'
+    b = '(((2*(3+2))+5)/2)'
+    c = '(*2(1+2))'
+    print(validate(a))
     print(eval(a))
+    print(validate(b))
+    print(eval(b))
+    print(validate(c))
 
     menu()
