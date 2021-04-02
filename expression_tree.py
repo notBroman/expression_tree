b41c430f92a0ee7036deaf430cc73b95a9a4f6e7
notBroman <notbroman@gmail.com>
2021-04-02 15:27:47 +0200

    added test, ported everything into 1 file, exception handling when loading from a file

--- expression_tree.py
+++ expression_tree.py
@@ -1,7 +1,6 @@
 """ 52093080 """
 import re
-import pickle
-from ADT import stack
+import unittest
 
 """ error classes """
 # this lets me catch the exception, when looking for invalid expressions
@@ -24,6 +23,147 @@ class OperatorsError(Exception):
     # operator is places incorrectly
     pass
 
+""" classes for testing: tree conversion, stack implementation, validation of expressions"""
+
+class test_tree(unittest.TestCase):
+    def test_buildtree(self):
+        t1 = _ExpressionTree("(((1+9)*3)-8)")
+        t1.build_tree()
+        t2 = _ExpressionTree("(((1+9)*4)-(4+8))")
+        t2.build_tree()
+
+        self.assertIsInstance(t1.build_tree(), _TreeNode)
+        self.assertEqual(t1.root.data, "-")
+        self.assertEqual(t1.inorder_trav(t1.root, []), ["1","+","9","*","3","-","8"])
+
+        self.assertIsInstance(t1.build_tree(), _TreeNode)
+        self.assertEqual(t2.root.data, "-")
+        self.assertEqual(t2.inorder_trav(t2.root, []), ["1","+","9","*","4","-","4","+","8"])
+
+class test_stack(unittest.TestCase):
+    def test_push(self):
+        s1 = Stack()
+
+        expression = "(((1+9)*4)-(4+8))"
+        for char in expression:
+            s1.push(char)
+            self.assertEqual(s1.get_top(), char)
+
+    def test_pop(self):
+        s1 = Stack()
+
+        expression = "(((1+9)*4)-(4+8))"
+        for char in expression:
+            if char != ")":
+                s1.push(char)
+            else:
+                oldsize = len(s1)
+                oldtop = s1.get_top()
+                s1.pop()
+                self.assertNotEqual(len(s1), oldsize)
+                self.assertNotEqual(s1.get_top(), oldtop)
+
+class test_valid(unittest.TestCase):
+    def test_validate(self):
+        self.assertFalse(validate("(4*3*2)"))
+        self.assertFalse(validate("(4*(2))"))
+        self.assertFalse(validate("(4*(3+2)*(2+1))"))
+        self.assertFalse(validate("(2*4)+(3+2)"))
+        self.assertFalse(validate("((2+3)*(4*5)"))
+        self.assertFalse(validate("(((2+3)*(4*5))+(1(2+3)))"))
+        self.assertFalse(validate("2"))
+        self.assertFalse(validate("((1+1)2*)"))
+        self.assertFalse(validate("!@#$^%&{:>>?<\|}"))
+        self.assertFalse(validate("(+2(1+1))"))
+
+""" stack implmentation with sinlgy linked list"""
+
+class _Node():
+    __slots__ = '_element', 'next', 'prev'
+
+    def __init__(self, element, next_node=None, prev_node=None):
+        self._element = element
+        self.next = next_node
+        self.prev = prev_node
+
+    def __str__(self):
+        try:
+            return str(self._element)
+        except Exception as e:
+            return e
+
+class Singly_Linked_List():
+
+    def __init__(self, values=None):
+        self._tail = None
+        self._head = None
+        if values is not None:
+            self.add_multiple(values)
+
+    def __iter__(self):
+        current = self._head
+        while current:
+            yield current
+            current = current.next
+
+    def __str__(self):
+        values = [str(x) for x in self]
+        return ' -> '.join(values)
+
+    def __len__(self):
+        length = 0
+        for element in self.__iter__():
+            length += 1
+        return length
+
+    def add_first(self, value):
+        newest = _Node(value)
+        if self._head is None:
+            newest.next = None
+            self._head = self._tail = newest
+        else:
+            newest.next = self._head
+            self._head = newest
+
+    def rm_first(self):
+        if self._head is None:
+            raise ValueError("Value Error: No elements in Linked List")
+        else:
+            removed = self._head
+            self._head = self._head.next
+
+""" Stack """
+class Stack():
+    def __init__(self):
+        self._data = Singly_Linked_List()
+        self._bot = self._data._tail
+        self._top = self._data._head
+        self._size = 0
+
+    def __str__(self):
+        return self._data.__str__()
+
+    def __len__(self):
+        return self._size
+
+    def pop(self):
+        if self._size == 0:
+            raise IndexError("Error: the stack is empty")
+        else:
+            popped = self._data._head._element
+            self._data.rm_first()
+            self._top = self._data._head
+            self._size -= 1
+            return popped
+
+    def push(self, element):
+       self._data.add_first(element)
+       self._size += 1
+
+    def get_top(self):
+        return self._data._head._element
+
+
 """ Tree node base and utils"""
 # inspired by @julkar9 post "Program to convert Infix notation to Expression Tree"
 # on geeksforgeeks.org
@@ -43,28 +183,24 @@ class _TreeNode():
             return self.data
 
 class _ExpressionTree():
-    def __init__(self, root:_TreeNode):
-        self.root = root
+    def __init__(self, expr:str):
+        self.root = None
+        self.expr = expr
 
     def __str__(self) -> str:
-        return str(inorder_trav(self.root, []))
+        return self.format_tree()
 
-    def print(self):
-        lst = inorder_trav(self.root, [])
-        print(lst)
-        return lst
+    def inorder_trav(self, start:_TreeNode, travd:list) -> list:
+        """ traverses left->root->right """
+        if start:
+            self.inorder_trav(start.left, travd)
+            travd.append(start.data)
+            self.inorder_trav(start.right, travd)
 
-def inorder_trav(start:_TreeNode, travd:list) -> list:
-    """ traverses left->root->right """
-    if start:
-        inorder_trav(start.left, travd)
-        travd.append(start.data)
-        inorder_trav(start.right, travd)
-
-        return travd
+            return travd
 
 
-def build_tree(string:str) -> _TreeNode:
+    def build_tree(self) -> _TreeNode:
         """ traverses the string an builds the expression tree while doing so """
         # traverses the string from left to right
         # all operators prioritze the left operand, so this should work fine
@@ -78,12 +214,13 @@ def build_tree(string:str) -> _TreeNode:
         #   pop the operator and the the opened bracket below it
         #   push the newly created node on the operand stack
 
+        string = self.expr
         for ch in string:
             if ch == " ":
                 ch.replace(" ","")
 
-        op_stack = stack.Stack()
-        node_stack = stack.Stack()
+        op_stack = Stack()
+        node_stack = Stack()
 
         p1 = re.compile("[\(\+\-\*\/]")
 
@@ -96,6 +233,7 @@ def build_tree(string:str) -> _TreeNode:
                 op_stack.push(ch)
 
             elif ch == ")":
+                """ convert operand pair and operator to node """
                 parent = _TreeNode(op_stack.pop())
 
                 parent.right = node_stack.pop()
@@ -104,29 +242,33 @@ def build_tree(string:str) -> _TreeNode:
 
                 op_stack.pop()
 
-                # print("new")
-                # print(op_stack)
-                # print(node_stack)
+        self.root = node_stack.pop()
+        return self.root
 
-            else:
-                raise Exception("Error: unxpected character in expression")
-
-        return node_stack.pop()
-
-def format_tree(string:str) -> str:
-    s1 = stack.Stack()
-    ops = ["+","-","*","/"]
-    out = ""
-    for char in string:
-        if char == "(":
-            s1.push(char)
-        elif char == ")":
-            s1.pop()
-        elif char.isnumeric():
-            out += "\t"*len(s1) + char + "\n"
-        elif char in ops:
-            out += "\t"*(len(s1)-1) + char + "\n"
-    return out
+    def format_tree(self) -> str:
+        """ get tree in decent format for printing to user """
+        # the depth can be depermined by counting the brackets
+        # ( -> depth++
+        # ) -> depth--
+        # operators are always at depth of operand - 1
+        # if open brackets are push onto a stack the size of the stack is the depth of the operand
+        # when brackets are closed pop an opened bracket from the stack
+        # add tabulators \t * depth to string for operands, then operand, then newline \n
+        # add tabulators \t * (depth--) to string for operators, then operator, then newline \n
+        string = self.expr
+        s1 = Stack()
+        ops = ["+","-","*","/"]
+        out = ""
+        for char in string:
+            if char == "(":
+                s1.push(char)
+            elif char == ")":
+                s1.pop()
+            elif char.isnumeric():
+                out += "\t"*len(s1) + char + "\n"
+            elif char in ops:
+                out += "\t"*(len(s1)-1) + char + "\n"
+        return out
 
 
 
@@ -193,7 +335,7 @@ def num_brackets(string:str) -> Exception:
         raise OperandsError("invalid expression: wrong number of brackets/operators")
 
 
-def match_bracket(st1:stack, string:str) -> Exception:
+def match_bracket(st1:Stack, string:str) -> Exception:
     """ use a stack to see if brackets are closed"""
     # use a comprehension to filter out brackets
     brackets = [str(e) for e in string if e == "(" or e == ")"]
@@ -217,7 +359,7 @@ def validate(string:str)->bool:
     # errors in the expression if there are
     # this approach was choosen to prioritize readability and expandability
     # also streamlines the interface of all the validation functions
-    s1 = stack.Stack()
+    s1 = Stack()
     try:
         match_bracket(s1, string)
         num_brackets(string)
@@ -233,10 +375,12 @@ def validate(string:str)->bool:
 
 """ saving and loading from a .json file """
 def save(string:str, name="save.txt"):
+    """ save to a save.txt file in the same directory as the .py file"""
     with open(name, mode="a", encoding="utf-8") as file:
         file.write(string + "\n")
 
 def load(name:str="save.txt"):
+    """ load from save.txt file, in the same directory as the .py file"""
     with open(name, mode="r", encoding="utf-8") as file:
         lst = (file.readlines())
         lst = [e.replace("\n","") for e in lst]
@@ -263,9 +407,8 @@ def menu():
                 if valid == True:
                     print("expression is valid")
                     print(f"{usr_in} = {eval(usr_in)}\n")
-                    r1 = build_tree(usr_in)
-                    t1 = _ExpressionTree(r1)
-                    print(format_tree(usr_in))
+                    t1 = _ExpressionTree(usr_in)
+                    print(t1)
 
                     input_string = input("Do You want to save this tree? (Y/n)")
                     input_string = input_string.replace(" ","")
@@ -280,31 +423,31 @@ def menu():
 
 
             elif option == 2:
-                entries = load()
-                print(entries)
-                input_string = input("Do You want to print a saved tree? (Y/n)")
-                input_string = input_string.replace(" ", "")[:1]
-            if input_string == "Y" or input_string == "y":
-                for e in entries:
-                    print(e + "\n")
-                    print(format_tree(e) + "\n")
-            else:
-                print("no trees were printed")
+                try:
+                    entries = load()
+                except Exception:
+                    print("error while loading, check if save.txt exists")
+                else:
+                    entries = [e for e in entries if validate(e) == True]
+                    print(entries)
+                    input_string = input("Do You want to print a saved tree? (Y/n)")
+                    input_string = input_string.replace(" ", "")[:1]
+                    if input_string == "Y" or input_string == "y":
+                        for e in entries:
+                            print(e + "\n")
+                            t = _ExpressionTree(e)
+                            print(t)
+                    else:
+                        print("no trees were printed")
 
 if __name__ == '__main__':
     # calling menu to run the program, ideally this would be compiled into a .exe or .deb
     # to not have to run the source code manually, this would make it more portable
     # packages would not need to be installed by the end user
-    a = '(((5 + 2) * (2 - 1))/((2 + 9) + ((7 - 2) - 1)) * 8)'
-    b = '(((2*(3+2))+5)/2)'
-    c = '(((2*(3+2))+5)/2)'
 
-    # print("----------------Tree test-----------------")
-    # t1 = _ExpressionTree(build_tree(c))
-    # root = build_tree(c)
-    # print(root)
-    # print("\n")
-    # print(t1)
-    # print(format_tree(c))
+    # uncomment line 465 to run tests
+    #
+    # uncomment line 463 to run program normally
 
+    # unittest.main()
     menu()
